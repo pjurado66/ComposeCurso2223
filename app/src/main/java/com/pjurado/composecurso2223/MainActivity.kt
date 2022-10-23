0d2753d2f5a57845d4427f8cbbb898c5fa11e3ae
[pjurado66] <[pjurado@ieshnosmachado.org]>
2022-10-23 07:55:28 +0200

    MediaItems

--- app/src/main/java/com/pjurado/composecurso2223/MainActivity.kt
+++ app/src/main/java/com/pjurado/composecurso2223/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -17,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pjurado.composecurso2223.ui.theme.ComposeCurso2223Theme
 
 class MainActivity : ComponentActivity() {
@@ -36,34 +34,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-@Preview(
-    showBackground = true,
-    name = "Compose Preview",
-    widthDp = 400,
-    heightDp = 200
-)
+@Preview
 @Composable
-fun DefaultPreview() {
-    ComposeCurso2223Theme {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally
+fun mediaItem(){
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .background(Color.Red)
         ) {
-            Greeting(
-                name = "Android",
-                modifier = Modifier
-                    .background(Color.Yellow)
-                    .weight(1f)
-            )
-            Greeting(
-                name = "Android",
-                modifier = Modifier
-                    .background(Color.Green)
-                    .weight(2f)
-            )
+            
         }
+        Text(text = "Title 1")
     }
 }
 
