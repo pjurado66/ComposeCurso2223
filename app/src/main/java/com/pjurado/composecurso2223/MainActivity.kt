f4fc105217b05bc98a3bbca486ade8637741caab
[pjurado66] <[pjurado@ieshnosmachado.org]>
2022-11-01 18:54:46 +0100

    Introducción completa

--- app/src/main/java/com/pjurado/composecurso2223/MainActivity.kt
+++ app/src/main/java/com/pjurado/composecurso2223/MainActivity.kt
@@ -1,22 +1,51 @@
 package com.pjurado.composecurso2223
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.PlayCircleOutline
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
+import com.pjurado.composecurso2223.MediaItem.*
 import com.pjurado.composecurso2223.ui.theme.ComposeCurso2223Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +58,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MediaList()
                 }
             }
         }
@@ -38,15 +67,48 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun mediaItem(){
-    Column {
+fun MediaList(){
+    LazyVerticalGrid(
+        contentPadding = PaddingValues(2.dp),
+        columns = GridCells.Adaptive(150.dp)
+    ) {
+        items(getMedia()){ item->
+            MediaListItem(item, Modifier.padding(2.dp))
+        }
+    }
+}
+
+//@Preview
+@Composable
+fun MediaListItem(item: MediaItem, modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier
+    ) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(200.dp)
-                .background(Color.Red)
         ) {
-            
+
+            Image(
+                painter = rememberImagePainter(
+                    data = item.photo
+                ),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+            if (item.type == Type.VIDEO) {
+                Icon(
+                    imageVector = Icons.Default.PlayCircleOutline,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier
+                        .size(92.dp)
+                        .align(Alignment.Center)
+                )
+            }
+
         }
         Box(
             contentAlignment = Alignment.Center,
@@ -55,17 +117,23 @@ fun mediaItem(){
                 .background(MaterialTheme.colors.secondary)
                 .padding(8.dp)
         ){
-            Text(text = "Title 1")
+            Text(
+                text = item.title,
+                style = MaterialTheme.typography.h4
+            )
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
+    Text(
+        text = name,
+        modifier = modifier
+    )
 }
 
-//@Preview(showBackground = true, widthDp = 200, heightDp = 100)
+//@Preview(showBackground = true, widthDp = 300, heightDp = 100)
 @Composable
 fun ButtonText() {
     Box(
@@ -73,12 +141,25 @@ fun ButtonText() {
         contentAlignment = Alignment.Center
     ){
         Text(
-            text = "Hello World",
-            modifier = Modifier
-                .clickable { /*TODO*/ }
-                .background(Color.Cyan)
-                .border(width = 2.dp, color = Color.Blue)
-                .padding(horizontal = 16.dp, vertical = 8.dp)
+            text = stringResource(id = R.string.textoLargo),
+            color = Color.Red,
+            fontSize = 25.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.W200,
+            fontFamily = FontFamily.Monospace,
+            letterSpacing = 5.sp,
+            textDecoration = TextDecoration.Underline,
+            textAlign = TextAlign.Right,
+            lineHeight = 2.em,
+            maxLines = 1,
+            softWrap = false,
+            style = MaterialTheme.typography.h4.copy(
+                shadow = Shadow(
+                    offset = Offset(10f, 10f),
+                    blurRadius = 10f
+                )
+            )
+
         )
     }
 }
---
+++ app/src/main/java/com/pjurado/composecurso2223/MediaItem.kt
@@ -0,0 +1,21 @@
+package com.pjurado.composecurso2223
+
+import com.pjurado.composecurso2223.MediaItem.Type
+
+data class MediaItem(
+    val id: Int,
+    val title: String,
+    val photo: String,
+    val type: Type
+){
+    enum class Type{PHOTO, VIDEO}
+}
+
+fun getMedia() = (1..100).map{
+    MediaItem(
+        id = it,
+        title = "Título $it",
+        photo = "https://loremflickr.com/240/320/people?lock=$it/",
+        type = if (it % 3 == 0) Type.VIDEO else Type.PHOTO
+    )
+}
