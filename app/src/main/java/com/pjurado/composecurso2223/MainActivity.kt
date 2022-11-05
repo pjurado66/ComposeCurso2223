5bb1e666f800a1c7ae5c0b8cb4ab234652f80d47
[pjurado66] <[pjurado@ieshnosmachado.org]>
2022-11-05 10:01:30 +0100

    State

--- app/src/main/java/com/pjurado/composecurso2223/MainActivity.kt
+++ app/src/main/java/com/pjurado/composecurso2223/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.pjurado.composecurso2223
 
 
+import android.graphics.drawable.StateListDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,14 +14,12 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.PlayCircleOutline
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,6 +36,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
@@ -58,14 +58,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MediaList()
+                    val (value, onValueChange) = rememberSaveable{mutableStateOf("")}
+                    StateSample(
+                        value = value,
+                        onValueChange = { onValueChange }
+                    )
                 }
             }
         }
     }
 }
 
-@Preview
+@Composable
+fun StateSample(value: String, onValueChange: (String) -> Unit){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(64.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = value,
+            onValueChange = { onValueChange(it) },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = value,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Yellow)
+                .padding(8.dp)
+        )
+        Button(
+            onClick = { onValueChange("") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = value.isNotEmpty()
+        ) {
+            Text(text = "Clear")
+        }
+    }
+}
+
+
+//@Preview
 @Composable
 fun MediaList(){
     LazyVerticalGrid(
